af1300152b098e0edf3149d2c6dc76f667d09eb2
Bach Nguyen <bachnq214@gmail.com>
2020-09-28 09:46:22 +0700

    policy

--- Modules/ThongTinChung/Policies/DepartmentPolicy.php
+++ Modules/ThongTinChung/Policies/DepartmentPolicy.php
@@ -29,48 +29,37 @@ class DepartmentPolicy extends BasePolicy
         //
     }
 
-    public function list(User $user)
-    {
-        return AuthHelper::can($user, 'key_officer');
+//
+//    public function update(User $user, Department $model = null)
+//    {
+//        $a = AuthHelper::can($user, 'key_officer');
+//        if (!AuthHelper::isSuperAdmin($user) || !AuthHelper::isAdmin($user)) {
+//            $b = $user->university_id == $model->university_id;
+//        } else {
+//            $b = true;
+//        }
+//        return $a && $b;
+//    }
 
-    }
-
-    public function create(User $user)
-    {
-        return AuthHelper::can($user, 'key_officer');
-
-    }
-
-    public function update(User $user, Department $model = null)
-    {
-        $a = AuthHelper::can($user, 'key_officer');
-        if (!AuthHelper::isSuperAdmin($user) || !AuthHelper::isAdmin($user)) {
-            $b = $user->university_id == $model->university_id;
-        } else {
-            $b = true;
-        }
-        return $a && $b;
-    }
-
-    public function view(User $user, Department $model)
-    {
-        $a = AuthHelper::can($user, 'key_officer');
-        if (!AuthHelper::isSuperAdmin($user) || !AuthHelper::isAdmin($user)) {
-            $b = $user->university_id == $model->university_id;
-        } else {
-            $b = true;
-        }
-        return $a && $b;
-    }
-
-    public function delete(User $user, Department $department)
-    {
-        $a = AuthHelper::can($user, 'key_officer');
-        if (!AuthHelper::isSuperAdmin($user) && !AuthHelper::isAdmin($user)) {
-            $b = $user->university_id == $department->university_id;
-        } else {
-            $b = true;
-        }
-        return $a && $b;
-    }
+//    public function view(User $user, Department $model)
+//    {
+//        $a = AuthHelper::can($user, 'key_officer');
+//        if (!AuthHelper::isSuperAdmin($user) || !AuthHelper::isAdmin($user)) {
+//            $b = $user->university_id == $model->university_id;
+//        } else {
+//            $b = true;
+//        }
+//        return $a && $b;
+//    }
+//
+//    public function delete(User $user, Department $department)
+//    {
+//        $a = AuthHelper::can($user, 'key_officer');
+//        if (!AuthHelper::isSuperAdmin($user) && !AuthHelper::isAdmin($user)) {
+//            $b = $user->university_id == $department->university_id;
+//        } else {
+//            $b = true;
+//        }
+//        return $a && $b;
+//    }
 }
