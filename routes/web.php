77a64ce00532b9792ded28c30abffdd381ac61d9
Bach Nguyen <bachnq214@gmail.com>
2020-10-09 09:55:29 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('frontend::index');
 });
+
+\Illuminate\Support\Facades\Route::any('/webhook', function (\Illuminate\Http\Request $request) {
+   \Illuminate\Support\Facades\Log::info($request->method());
+   \Illuminate\Support\Facades\Log::info(json_encode($request->all()));
+   \Illuminate\Support\Facades\Log::info(json_encode($request->header()));
+//   \Illuminate\Support\Facades\Log::info(json_encode($request->));
+});
