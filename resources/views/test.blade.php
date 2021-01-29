a87880ed04cc392e2d32ce16c74e43ba2d6b882e
bachnguyen <bachnq214@gmail.com>
2021-01-29 16:59:02 +0700

    Test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,579 @@
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.5.0/css/flag-icon.min.css"
+          integrity="sha512-Cv93isQdFwaKBV+Z4X8kaVBYWHST58Xb/jVOcV9aRsGSArZsgAnFIhMpDoMDcFNoUtday1hdjn0nGp3+KZyyFw=="
+          crossorigin="anonymous"/>
+
+</head>
+<body>
+<style>
+    .h1-header {
+        padding-bottom: 10px;
+        border-bottom: 3px solid #000000;
+    }
+
+    .stat-box {
+        border: 1px solid #aaa;
+        text-align: center;
+    }
+
+    .display-block {
+        display: block;
+    }
+
+    .left {
+        float: left;
+    }
+
+    .right {
+        float: right;
+    }
+
+    .right:after {
+        clear: both;
+    }
+
+    .stat-box p {
+        padding-top: 10px;
+    }
+
+    .stat-box h3 {
+        padding-bottom: 10px;
+    }
+
+    .rate-row {
+        margin: 20px 0px;
+    }
+
+    .open-table {
+        margin-top: 30px;
+    }
+
+    .subscriber-activity {
+        margin-top: 50px;
+    }
+
+    .perform24h {
+        margin-top: 30px;
+    }
+    .open-table tr {
+        line-height: 30px;
+        min-height: 30px;
+        height: 30px;
+    }
+
+</style>
+<div class="container">
+    <div class="overview">
+        <div class="header">
+            <h1 class="h1-header">Overview</h1>
+        </div>
+        <div class="overview">
+            <div class="row">
+                <div class="col-12" style="padding: 20px 5px">
+                    <h1>Campaign: Mừng năm mới</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-6">
+                    <p><b>Sender: </b> Hello World</p>
+                    <p><b>Subject: </b> Hello World</p>
+                </div>
+                <div class="col-6">
+                    <p><b>Delivery Time: </b> 20-11-2022 14:20:50</p>
+                </div>
+            </div>
+
+            <div class="row stat-row">
+                <div class="col-3 stat-box">
+                    <p>Open</p>
+                    <h3>10</h3>
+                </div>
+                <div class="col-3 stat-box">
+                    <p>Click</p>
+                    <h3>10</h3>
+                </div>
+                <div class="col-3 stat-box">
+                    <p>Bounce</p>
+                    <h3>10</h3>
+                </div>
+                <div class="col-3 stat-box">
+                    <p>Unsubscribed</p>
+                    <h3>10</h3>
+                </div>
+            </div>
+            <div class="row rate-row">
+                <div class="col-6 open-rate">
+                    <p><b>Open rate</b> <span class="right">80%</span></p>
+                    <div class="progress" style="clear: both; margin-bottom: 1rem">
+                        <div class="progress-bar" style="width: 80%" role="progressbar" aria-valuenow="80"
+                             aria-valuemin="0"
+                             aria-valuemax="100"></div>
+                    </div>
+                    <p>
+                        Successful deliveries
+                        <span class="right">80%</span>
+                    </p>
+                    <p>
+                        Total open
+                        <span class="right">80%</span>
+                    </p>
+                    <p>
+                        Last Open
+                        <span class="right">20-1-2021 19:20:13</span>
+                    </p>
+                    <p>
+                        Forwarded
+                        <span class="right">20</span>
+                    </p>
+                </div>
+                <div class="col-6">
+                    <p><b>Click rate</b> <span style="float: right">65.4%</span></p>
+                    <div class="progress" style="clear: both; margin-bottom: 1rem">
+                        <div class="progress-bar" style="clear: both; margin-bottom: 1rem" role="progressbar"
+                             aria-valuenow="65.4"
+                             aria-valuemin="0"
+                             aria-valuemax="100"></div>
+                    </div>
+                    <p>
+                        Clicks per unique opens
+                        <span class="right">80%</span>
+                    </p>
+                    <p>
+                        Total clicks
+                        <span class="right">80%</span>
+                    </p>
+                    <p>
+                        Last clicked
+                        <span class="right">20-1-2021 19:20:13</span>
+                    </p>
+                    <p>
+                        Abuse reports
+                        <span class="right">0</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="open-by-location">
+        <div class="header">
+            <h1 class="h1-header">Opens by location</h1>
+        </div>
+        <div class="open-location">
+            <table class="table table-striped open-table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Country</th>
+                    <th>Opens</th>
+                    <th>Percents</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td><span class="flag-icon flag-icon-vn"></span> Viet Nam</td>
+                    <td>20</td>
+                    <td>100%</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><span class="flag-icon flag-icon-vn"></span> Viet Nam</td>
+                    <td>20</td>
+                    <td>100%</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><span class="flag-icon flag-icon-vn"></span> Viet Nam</td>
+                    <td>20</td>
+                    <td>100%</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><span class="flag-icon flag-icon-vn"></span> Viet Nam</td>
+                    <td>20</td>
+                    <td>100%</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><span class="flag-icon flag-icon-vn"></span> Viet Nam</td>
+                    <td>20</td>
+                    <td>100%</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td><span class="flag-icon flag-icon-vn"></span> Viet Nam</td>
+                    <td>20</td>
+                    <td>100%</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="subscriber-activity">
+        <div class="header">
+            <h1 class="h1-header">Subscriber activity</h1>
+        </div>
+        <div class="perform24h">
+            <h1>24-hour performance</h1>
+            <div style="width:100%; height: 400px">
+                <canvas id="canvas"></canvas>
+            </div>
+        </div>
+        <div class="most-subscriber">
+            <h1>Subscribers with most active</h1>
+            <table class="table table-striped open-table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Email</th>
+                    <th>Open</th>
+                    <th>Click</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>bachnq214@gmail.com</td>
+                    <td>20</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>bachnq214@gmail.com</td>
+                    <td>20</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>bachnq214@gmail.com</td>
+                    <td>20</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>bachnq214@gmail.com</td>
+                    <td>20</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>bachnq214@gmail.com</td>
+                    <td>20</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>bachnq214@gmail.com</td>
+                    <td>20</td>
+                    <td>10</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="most-link">
+            <h1>Top links clicked</h1>
+            <table class="table table-striped open-table">
+                <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Link</th>
+                    <th>Click</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>fb.com</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>fb.com</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>fb.com</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>fb.com</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>fb.com</td>
+                    <td>10</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>fb.com</td>
+                    <td>10</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+
+<script>
+    'use strict';
+
+    window.chartColors = {
+        red: 'rgb(255, 99, 132)',
+        orange: 'rgb(255, 159, 64)',
+        yellow: 'rgb(255, 205, 86)',
+        green: 'rgb(75, 192, 192)',
+        blue: 'rgb(54, 162, 235)',
+        purple: 'rgb(153, 102, 255)',
+        grey: 'rgb(201, 203, 207)'
+    };
+
+    (function(global) {
+        var MONTHS = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December'
+        ];
+
+        var COLORS = [
+            '#4dc9f6',
+            '#f67019',
+            '#f53794',
+            '#537bc4',
+            '#acc236',
+            '#166a8f',
+            '#00a950',
+            '#58595b',
+            '#8549ba'
+        ];
+
+        var Samples = global.Samples || (global.Samples = {});
+        var Color = Chart.helpers.color;
+
+        function applyDefaultNumbers(config) {
+            var cfg = config || {};
+            cfg.min = cfg.min || 0;
+            cfg.max = cfg.max || 1;
+            cfg.from = cfg.from || [];
+            cfg.count = cfg.count || 8;
+            cfg.decimals = cfg.decimals || 8;
+            cfg.continuity = cfg.continuity || 1;
+
+            return cfg;
+        }
+
+        Samples.utils = {
+            // Adapted from http://indiegamr.com/generate-repeatable-random-numbers-in-js/
+            srand: function(seed) {
+                this._seed = seed;
+            },
+
+            rand: function(min, max) {
+                var seed = this._seed;
+                min = min === undefined ? 0 : min;
+                max = max === undefined ? 1 : max;
+                this._seed = (seed * 9301 + 49297) % 233280;
+                return min + (this._seed / 233280) * (max - min);
+            },
+
+            numbers: function(config) {
+                var cfg = applyDefaultNumbers(config);
+                var dfactor = Math.pow(10, cfg.decimals) || 0;
+                var data = [];
+                var i, value;
+
+                for (i = 0; i < cfg.count; ++i) {
+                    value = (cfg.from[i] || 0) + this.rand(cfg.min, cfg.max);
+                    if (this.rand() <= cfg.continuity) {
+                        data.push(Math.round(dfactor * value) / dfactor);
+                    } else {
+                        data.push(null);
+                    }
+                }
+
+                return data;
+            },
+
+            labels: function(config) {
+                var cfg = config || {};
+                var min = cfg.min || 0;
+                var max = cfg.max || 100;
+                var count = cfg.count || 8;
+                var step = (max - min) / count;
+                var decimals = cfg.decimals || 8;
+                var dfactor = Math.pow(10, decimals) || 0;
+                var prefix = cfg.prefix || '';
+                var values = [];
+                var i;
+
+                for (i = min; i < max; i += step) {
+                    values.push(prefix + Math.round(dfactor * i) / dfactor);
+                }
+
+                return values;
+            },
+
+            months: function(config) {
+                var cfg = config || {};
+                var count = cfg.count || 12;
+                var section = cfg.section;
+                var values = [];
+                var i, value;
+
+                for (i = 0; i < count; ++i) {
+                    value = MONTHS[Math.ceil(i) % 12];
+                    values.push(value.substring(0, section));
+                }
+
+                return values;
+            },
+
+            color: function(index) {
+                return COLORS[index % COLORS.length];
+            },
+
+            transparentize: function(color, opacity) {
+                var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+                return Color(color).alpha(alpha).rgbString();
+            }
+        };
+
+        // DEPRECATED
+        window.randomScalingFactor = function() {
+            return Math.round(Samples.utils.rand(-100, 100));
+        };
+
+        // INITIALIZATION
+
+        Samples.utils.srand(Date.now());
+
+        // Google Analytics
+        /* eslint-disable */
+        if (document.location.hostname.match(/^(www\.)?chartjs\.org$/)) {
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-28909194-3', 'auto');
+            ga('send', 'pageview');
+        }
+        /* eslint-enable */
+
+    }(this));
+
+</script>
+<script>
+    window.chartColors = {
+        red: 'rgb(255, 99, 132)',
+        orange: 'rgb(255, 159, 64)',
+        yellow: 'rgb(255, 205, 86)',
+        green: 'rgb(75, 192, 192)',
+        blue: 'rgb(54, 162, 235)',
+        purple: 'rgb(153, 102, 255)',
+        grey: 'rgb(201, 203, 207)'
+    };
+    window.randomScalingFactor = function() {
+        return Math.round(Samples.utils.rand(0, 20));
+    };
+    var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var config = {
+        type: 'line',
+        data: {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Open',
+                backgroundColor: window.chartColors.red,
+                borderColor: window.chartColors.red,
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor()
+                ],
+                fill: false,
+            }, {
+                label: 'Click',
+                fill: false,
+                backgroundColor: window.chartColors.blue,
+                borderColor: window.chartColors.blue,
+                data: [
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor(),
+                    randomScalingFactor()
+                ],
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Chart.js Line Chart'
+                },
+                tooltip: {
+                    mode: 'index',
+                    intersect: false,
+                }
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                x: {
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Month'
+                    }
+                },
+                y: {
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value'
+                    }
+                }
+            }
+        },
+
+    }
+    window.onload = function() {
+        var ctx = document.getElementById('canvas').getContext('2d');
+        window.myLine = new Chart(ctx, config);
+    };
+
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+
+</body>
+</html>
